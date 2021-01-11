cd3dd90217f6761545cc3192f9ed6006fefaa42e
Aleksandar <lakatos.aleksandar79@gmail.com>
2021-01-11 22:56:40 +0100

    Create http get request for API(countries)

--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
-  constructor() { }
+  readonly API_BASE = "https://disease.sh/v3/covid-19/countries";
+
+  constructor(private http: HttpClient) { }
+
+  getCountries(): Observable<HttpResponse<any>>{
+    return this.http.get(this.API_BASE, {observe: 'response'});
+  }
 }
