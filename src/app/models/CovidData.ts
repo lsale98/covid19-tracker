84c8521988ad425a4650799d38dd64b228cd6d3f
Aleksandar <lakatos.aleksandar79@gmail.com>
2021-01-12 19:40:44 +0100

    Create Covid data model

---
+++ src/app/models/CovidData.ts
@@ -0,0 +1,11 @@
+export interface ICovidData {
+    name: string,
+    flag: string,
+    continent: string,
+    cases: number,
+    todayCases: number,
+    recovered: number,
+    todayRecovered: number,
+    deaths: number,
+    todayDeaths: number
+}
---
+++ src/app/services/covid.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class CovidService {
+
+  constructor(private http: HttpClient) { }
+}
