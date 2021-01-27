b2656f0011db432edc4382dc333c9a97a758bfa5
Aleksandar <lakatos.aleksandar79@gmail.com>
2021-01-27 20:06:43 +0100

    Upgrade angular for deploy on heroku

--- src/app/covid-data/covid-data.component.spec.ts
+++ src/app/covid-data/covid-data.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CovidDataComponent } from './covid-data.component';
 
@@ -6,7 +6,7 @@ describe('CovidDataComponent', () => {
   let component: CovidDataComponent;
   let fixture: ComponentFixture<CovidDataComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CovidDataComponent ]
     })
